436224d400e1497a77d66de1fe0f999c90befff3
sean200707-droid <sean200707@example.com>
2025-12-03 22:41:34 +0900

    Add debug logging and test page

--- index.tsx
+++ index.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log('Script loaded!');
+
 const rootElement = document.getElementById('root');
+console.log('Root element:', rootElement);
+
 if (!rootElement) {
+  console.error('Root element not found!');
+  document.body.innerHTML = '<div style="color: white; padding: 50px; background: #1a202c;">ERROR: Root element not found</div>';
   throw new Error("Could not find root element to mount to");
 }
 
+console.log('Creating React root...');
 const root = ReactDOM.createRoot(rootElement);
+console.log('Rendering App...');
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+console.log('App rendered!');
